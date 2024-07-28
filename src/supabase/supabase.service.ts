8581a5d3cf5623371f398eecfa5eafff08ecc33d
Farhan <farhan@aththariq.com>
2024-07-29 00:01:48 +0700

    chore: Generate Supabase typescript definitions

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-// import { supabaseConfig } from '../config/supabase.config';
+import { Database } from './supabase.types';
 
 @Injectable()
 export class SupabaseService {
-  private supabase: SupabaseClient;
+  private supabase: SupabaseClient<Database>;
 
   constructor(private configService: ConfigService) {
-    this.supabase = createClient(
+    this.supabase = createClient<Database>(
       this.configService.get<string>('SUPABASE_URL'),
       this.configService.get<string>('SUPABASE_KEY'),
     );
