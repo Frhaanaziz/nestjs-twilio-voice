86306d2542e787d4c94a12122a08ff68f81dc942
Farhan <farhan@aththariq.com>
2024-08-01 22:09:05 +0700

    feat: Update TwilioController to use GenerateTwilioAccessTokenDto

--- src/twilio/twilio.controller.ts
+++ src/twilio/twilio.controller.ts
@@ -1,18 +1,14 @@
 import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { TwilioService } from 'src/twilio/twilio.service';
-import { GenerateAccessTokenDto } from './dto/generate-access-token.dto';
+import { GenerateTwilioAccessTokenDto } from './dto/generate-twilio-access-token.dto';
 
 @Controller('twilio')
 export class TwilioController {
   constructor(private readonly twilioService: TwilioService) {}
 
   @Post('generate-access-token')
-  async generateAccesToken(
-    @Body() generateAccessTokenDto: GenerateAccessTokenDto,
-  ) {
-    const token = await this.twilioService.generateVoiceAccessToken({
-      userId: generateAccessTokenDto.user_id,
-    });
+  async generateAccesToken(@Body() body: GenerateTwilioAccessTokenDto) {
+    const token = await this.twilioService.generateVoiceAccessToken(body);
 
     return { data: { token } };
   }
