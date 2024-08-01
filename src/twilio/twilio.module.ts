d5f06f45e97b3b264bb1d959e65ba40296a17379
Farhan <farhan@aththariq.com>
2024-08-01 22:08:53 +0700

    feat: Add CallLogsModule to Twilio module imports

--- src/twilio/twilio.module.ts
+++ src/twilio/twilio.module.ts
@@ -3,8 +3,10 @@ import { TwilioService } from './twilio.service';
 import { ConfigService } from '@nestjs/config';
 import { SupabaseService } from 'src/supabase/supabase.service';
 import { TwilioController } from './twilio.controller';
+import { CallLogsModule } from 'src/call-logs/call-logs.module';
 
 @Module({
+  imports: [CallLogsModule],
   controllers: [TwilioController],
   providers: [TwilioService, ConfigService, SupabaseService],
   exports: [TwilioService],
